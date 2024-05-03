e6293fc8aaeaf7a4e226a0eeae678d669f2553c5
Ashu7891 <nikhilashish97@gmail.com>
2024-05-03 21:26:57 +0530

    update

--- Death Wish Coffee Website/productDescr.js
+++ Death Wish Coffee Website/productDescr.js
@@ -1,7 +1,6 @@
 // for first slider, second slider, Coffee Page, merch page and bunbles Page put into description page
 window.addEventListener("load", ()=>{
     const params = new URLSearchParams(window.location.search)
-    console.log(params)
     const title = params.get("title")
     const price = params.get("price")
     const image = params.get("image")
@@ -9,7 +8,6 @@ window.addEventListener("load", ()=>{
     const star = params.get("star")
     const rating = params.get("rating")
     const id = params.get("id")
-    console.log(id)
 
     document.getElementById("image").src = image
     document.getElementById("title").innerText = title
@@ -19,5 +17,20 @@ window.addEventListener("load", ()=>{
     document.getElementById("star").innerHTML = star
     document.getElementById("rating").innerText = rating
     document.getElementById("add-cart").setAttribute("data-id",id)
-    document.getElementById("add-cart").classList.add("addCartButton", "addSecondSlider", "addMerchDataButton", "addBundleData");
+    console.log(id)
+    if(id>=0 && id<=8){
+        document.getElementById("add-cart").setAttribute("class","addCartButton")
+    }
+    if(id>=9 && id<=17){
+        document.getElementById("add-cart").setAttribute("class","addSecondSlider")
+    }
+    if(id>=18 && id<=37){
+        document.getElementById("add-cart").setAttribute("class","addCoffeeDataButton")
+    }
+    if(id>=38 && id<=85){
+        document.getElementById("add-cart").setAttribute("class","addMerchDataButton")
+    }
+    if(id>=86 && id<=103){
+        document.getElementById("add-cart").setAttribute("class","addBundleData")
+    }
 })
